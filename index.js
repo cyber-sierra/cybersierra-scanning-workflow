179cbc3897ed78e95416f951b787cb27c859c70a
khoahuynhdev <khoa0319@gmail.com>
2022-08-02 15:10:28 +0700

    init

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const core = require('@actions/core');
 const http = require('@actions/http-client');
 
+const PROCESSING_STATUS = 'Scanning';
+const SCAN_INTERVAL_CHECK = 5000;
+const CI_SCAN_TIMEOUT = 1000 * 60 * 15; // 15 minutes timeout
+
 async function triggerScan(scanTarget) {
   core.info('Scan target: ' + scanTarget);
-  const scanUrl = `${process.env.CS_SCAN_URL}/scan/ci`;
+  const scanUrl = `${process.env.CS_SCAN_URL}/ci/scan`;
+  core.debug('Scan URL: ' + scanUrl);
   const client = new http.HttpClient();
   const jsonObj = await client.postJson(
     scanUrl,
@@ -34,19 +39,15 @@ async function triggerScan(scanTarget) {
 }
 
 async function getScanResult(scanId) {
-  const scanUrl = `${process.env.CS_SCAN_URL}/scan-ci/${scanId}/overview`;
+  const scanUrl = `${process.env.CS_SCAN_URL}/ci/scan/${scanId}/overview`;
   const client = new http.HttpClient();
   // set interval to check scan status every 5 seconds
   // timeout in 15 minutes
   const intervalId = setInterval(async () => {
-    const jsonObj = await client.getJson(
-      scanUrl,
-      {},
-      {
-        'Content-Type': 'application/json',
-        'x-api-key': process.env.CS_API_TOKEN,
-      },
-    );
+    const jsonObj = await client.getJson(scanUrl, {
+      'Content-Type': 'application/json',
+      'x-api-key': process.env.CS_API_TOKEN,
+    });
     if (jsonObj.statusCode !== 200)
       return core.setFailed('Failed to get scan result');
     if (
@@ -55,18 +56,32 @@ async function getScanResult(scanId) {
       jsonObj.result.meta.statusCode === 200
     ) {
       core.info(JSON.stringify(jsonObj.result.response));
-      if (jsonObj.result.response.subTotal[0].count > 0) {
+      if (jsonObj.result.response.status !== PROCESSING_STATUS) {
         clearInterval(intervalId);
-        return core.info('Scan completed');
+        core.info('Scan completed');
+        return summaryResult(jsonObj.result.response);
       }
       return core.info('Checking scan status...');
     }
-    return core.setFailed(jsonObj.result.meta.message);
-  }, 5000);
+    return core.setFailed(jsonObj.result.meta);
+  }, SCAN_INTERVAL_CHECK);
   setTimeout(() => {
     clearInterval(intervalId);
-  }, 900000);
+    core.setFailed('Scan timeout');
+  }, CI_SCAN_TIMEOUT);
 }
+
+function summaryResult(result) {
+  if (result.status === 'successful') {
+    if (result.critical > 0) {
+      core.setFailed('Scan completed with critical issues');
+    }
+    core.info('Scan completed successfully');
+    process.exit(0);
+  }
+  core.setFailed('Scan completed with issues');
+}
+
 async function run() {
   try {
     if (!process.env.CS_SCAN_URL)
